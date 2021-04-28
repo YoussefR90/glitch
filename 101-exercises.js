bce3b174d35e9d33f8ea17b572fd500e0baad8c7
Glitch (101-exercises) <none>
2021-04-28 18:37:43 +0000

    🎸👚 Checkpoint ./101-exercises.js:30990276/391

--- 101-exercises.js
+++ 101-exercises.js
@@ -106,7 +106,8 @@ addToDone("Exercise 8 is correct.")
 // Exercise 9
 // Write the code necessary to sort the fruits in reverse alphabetical order
 var fruits = ['tomato', 'strawberry', 'mango', 'kiwi', 'guava', 'banana'];
-fruits.sort()
+fruits.sort();
+fruits.reverse()
 assert(fruits, ['tomato', 'strawberry', 'mango', 'kiwi', 'guava', 'banana'], "Exercise 9")
 addToDone("Exercise 9 is correct.")
 
@@ -115,7 +116,7 @@ addToDone("Exercise 9 is correct.")
 // Write the code necessary to produce a single array that holds all fruits then all vegetables in the order as they were sorted above.
 // Assign the result to a variable named fruitsAndVeggies. 
 // *hint* the search engine search here would be "how to combine two arrays in JavaScript", for example.
-
+var fruits, vegetables = [fruitsandVeggies]
 assert(fruitsAndVeggies, ['tomato', 'strawberry', 'mango', 'kiwi', 'guava', 'banana', 'broccoli', 'carrot', 'cauliflower', 'eggplant', 'tomato', 'zucchini'], "Exercise 10")
 addToDone("Exercise 10 is correct")
 
